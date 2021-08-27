d25da16028c053b650f98c357c9011866db65230
Nguyễn Thanh Hải <85782033+NguyenThanhHai-palat@users.noreply.github.com>
2021-08-27 16:57:57 +0700

    Update inpuths.js

--- data/inpuths.js
+++ data/inpuths.js
@@ -3,35 +3,68 @@ function mhs() {
   var fruits = document.getElementById("puths").value;
 
   switch(fruits) {
-    case "7500076814000015":
-    text = "Nguyễn Tấn Bình An";
+    case "Nguyễn Tấn Bình An":
+    text = "7500076814000015";
     break;
-    case "1902195924":
-    text = "Chí Thái Tâm Anh";
+    case "Chí Thái Tâm Anh":
+    text = "1902195924";
     break;
-    case "1902195925":
-    text = "Phạm Bùi Đức Anh";
+    case "Phạm Bùi Đức Anh":
+    text = "1902195925";
     break;
-    case "1902195927":
-    text = "Bùi Trần Gia Bảo";
+    case "Bùi Trần Gia Bảo":
+    text = "1902195927";
     break;
-    case "1902195929":
-    text = "Trương Văn Công";
+    case "Trương Văn Công":
+    text = "1902195929";
     break;
-    case "1902195932":
-    text = "Nguyễn Thành Đạt (10/5)";
+    case "Nguyễn Thành Đạt (10/5)":
+    text = "1902195932";
     break;
-    case "1902195899":
-    text = "Nguyễn Thành Đạt (16/2)";
+    case "Nguyễn Thành Đạt (16/2)":
+    text = "1902195899";
     break;
-    case "1902195933":
-    text = "Nguyễn Thanh Hải";
+    case "Nguyễn Thanh Hải":
+    text = "1902195933";
     break;
-    case "1902195902":
-    text = "Dương Nhật Hằng";
+    case "Dương Nhật Hằng":
+    text = "1902195902";
     break;
-    case "1902195903":
-    text = "Huỳnh Vũ Ngọc Hân";
+    case "Huỳnh Vũ Ngọc Hân":
+    text = "1902195903";
+    break;
+    case "Trần Hữu Hiếu":
+    text = "1902195935";
+    break;
+    case "Trần Xuân Hiếu":
+    text = "1902195936";
+    break;
+    case "Bùi Minh Hoàng":
+    text = "1902195937";
+    break;
+    case "Dương Gia Huy":
+    text = "1902195938";
+    break;
+    case "Bùi Quang Hưng":
+    text = "1902195904";
+    break;
+    case "Phạm Nguyễn Minh Kha":
+    text = "1902195939";
+    break;
+    case "Lê Khanh":
+    text = "1902195940";
+    break;
+    case "Đào Bích Tường Lam":
+    text = "1902195941";
+    break;
+    case "Nguyễn Hoàng Khánh Lam":
+    text = "1902195907";
+    break;
+    case "Đặng Nhật Trúc Linh":
+    text = "7500076814000007";
+    break;
+    case "Trần Thiên Long":
+    text = "1902195908";
     break;
     default:
     text = "Vui Lòng Nhập Mã HS chính xác";
