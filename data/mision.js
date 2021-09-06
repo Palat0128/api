0a7088bbe64b149bd3a1aec99351e86a16263886
Nguyễn Thanh Hải <85782033+NguyenThanhHai-palat@users.noreply.github.com>
2021-09-06 11:00:51 +0700

    Create mision.js

---
+++ data/mision.js
@@ -0,0 +1,9 @@
+
+function trolai() {
+  window.history.back();
+}
+function QGNOBAN(){
+	let CountryNOTBAN = "Việt Nam";  // QUỐC GIA
+	document.getElementById("qgnotban").innerHTML =
+	CountryNOTBAN + " "; 
+}
