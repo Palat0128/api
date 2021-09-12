016a5a1b72da7f213efcc5b0ac27f04dd3c50bbb
Nguyễn Thanh Hải <85782033+NguyenThanhHai-palat@users.noreply.github.com>
2021-09-12 12:03:26 +0700

    Update mision.js

--- data/mision.js
+++ data/mision.js
@@ -71,4 +71,38 @@ function trinhduyet() {
   document.getElementById("vertd").innerHTML = v;
   document.getElementById("nentan").innerHTML = n;
 }
+function setCookie(cname,cvalue,exdays) {
+  const d = new Date();
+  d.setTime(d.getTime() + (exdays*24*60*60*1000));
+  let expires = "expires=" + d.toGMTString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(';');
+  for(let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+function checkCookie() {
+  let user = getCookie("username-part");
+  if (user != "") {
+    alert("Chào Mừng trở lại " + user);
+  } else {
+     user = prompt("Vui Lòng Nhập Tên Thật Của Bạn Trước Khi Vào Web:","");
+     if (user != "" && user != null) {
+       setCookie("username-part", user, 40);
+     }
+  }
+}
 //Contact with Palat in Nguyenthanhhai12072008@gmail.com
